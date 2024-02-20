53431ae3d65eadc42dbf32e753ac683b44d5da62
FSB-PC <homkkarsx@gmail.com>
2024-02-21 02:57:23 +0300

    Add docs

--- src/weather/dto/create-weather.dto.ts
+++ src/weather/dto/create-weather.dto.ts
@@ -1,19 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsDateString } from 'class-validator';
 
 export class CreateWeatherDto {
+  @ApiProperty({ example: 25, description: 'The temperature in Celsius' })
   @IsNumber()
   temperature: number;
 
+  @ApiProperty({ example: 80, description: 'The humidity percentage' })
   @IsNumber()
   humidity: number;
 
+  @ApiProperty({
+    example: 500,
+    description: 'The light intensity in Lumens',
+    required: false,
+  })
   @IsNumber()
   @IsOptional()
   light?: number;
 
+  @ApiProperty({
+    example: 1013,
+    description: 'The atmospheric pressure in hPa',
+  })
   @IsNumber()
   pressure: number;
 
+  @ApiProperty({
+    example: '2021-07-16T09:54:32.000Z',
+    description: 'The date and time of the weather data capture',
+    required: false,
+  })
   @IsDateString()
   @IsOptional()
   date?: string;
--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -8,18 +8,31 @@ import {
 } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import { CreateWeatherDto } from './dto/create-weather.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('weather')
 @Controller('weather')
 export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Create weather data' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The weather data has been successfully created.',
+  })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request.' })
   async create(@Body() createWeatherDto: CreateWeatherDto) {
     return this.weatherService.createWeatherData(createWeatherDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all weather data' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Weather data retrieved successfully.',
+  })
   async findAll() {
     return this.weatherService.getWeatherData();
   }
